3ecba2bfae342b3960d9a698ac2fa99e5b2ddced
Ivan Smyatkin <ismyatkin@gmail.com>
2020-05-25 17:13:15 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Иван Смяткин on 25.05.2020.
+//  Copyright © 2020 Иван Смяткин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
